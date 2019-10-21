d8ad7259f5f67fbb2e7c6c035b6a16b345e30551
pinerarenuncia <pinerarenuncia@protonmail.com>
2019-10-21 17:58:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# How to contribute
+ - Create your own fork of the project
+ - Edit the file `pages/index.vue`
+ - Add your `.mp4` video to the folder `assets/videos`. Please use a file name in spanish and descriptive (this will be used in the list of videos)
+ - Add your video at the very top of the array
+ - Send your PR
+
+# Como contribuir y reglas? En Espanol
+
+ - Haz un fork del proyecto
+ - Edita el archivo `pages/index.vue`
+ - Añade tu video en formato `.mp4` al directorio `assets/videos`, usa un nombre de archivo descriptivo
+ - Añade al princio del array `videos` tu video.
+ - Envia tu Pull Request
+
+# pinerarenuncia
+
+> Pruebas para que Pinera renuncie
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+```
+
+# Deploy to github page
+```
+# generate static project
+npm run generate
+# Deploy to github
+npm run deploy
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+# pinerarenuncia.github.io
---
+++ pages/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="links">
+    <div v-for="(video, index) in videos" :key="index" class="video-box">
+      <h4>{{video}}</h4>
+      <video controls :src="require('~/assets/videos/'+video)"></video>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      videos: [
+        "Detencion de carabineros usando un auto particular.mp4",
+        "carabineros y pdi dan ventaja para despues disparar por la espalda.mp4",
+        "carabineros persigue a una persona que esta grabando hasta su casa.mp4",
+        "Persona colgada en pedro aguirrez cerda.mp4",
+        "Acusan a carabineros infiltrados provocando disturbios en una feria.mp4",
+        "infiltrados disparan en Bulnes.mp4",
+        "zorron opinando del alza del pasaje del metro.mp4",
+        "lamada a radio biobio acerca de gente muerta en la noche de valparaiso 19 oct 2019.mp4",
+        "joven con bala en el brazo.mp4",
+        "carabinero acelera en su moto y cae.mp4",
+        "carabineros disparando a manifestantes en el centro de santiago.mp4",
+        "mujer con disparo en la cabeza tendida en el suelo.mp4",
+        "incendio en Monserrat con gente atratada.mp4",
+        "persona defiende a otro de carabineros en la reja de su casa.mp4",
+        "Militar le dispara a fotografo en el pie.mp4",
+        "furgon de carabineros superado por protestantes.mp4",
+        "vida normal en santiago oriente.mp4",
+        "Chadwick cagao de la risa.mp4",
+        "personas reanimando a joven baleado en La Serena.mp4",
+        "militares acarrean a joven inconsciente.mp4",
+        "joven en colina inconsciente.mp4",
+        "prensa filmando una protesta falsa.mp4",
+        "militares tienen a gente en el suelo dentro de un local.mp4",
+        "militares saliendo de aguas andinas.mp4",
+        "encapuchados aparecen de una patrulla de carabineros.mp4",
+        "civil encara a saqueadores.mp4",
+        "carabineros atropella a una persona en Lampa 4.mp4",
+        "carabineros atropella a una persona en Lampa 3.mp4",
+        "robo de auto en copec.mp4",
+        "carabinero le ordena a companero que dispare.mp4",
+        "senora en el suelo por disparo en la cabeza.mp4",
+        "joven atropellado en el piso.mp4",
+        "carabineros acelerando atropellando gente.mp4",
+        "joven muerto en lampa.mp4",
+        "carabineros consumiendo algo.mp4",
+        "Victor marileo es transportado hacia un helicoptero. Manalich no dice nada.mp4",
+        "vehiculo de carabineros atropella a un manifestante.mp4",
+        "carabinero baila.mp4",
+        "manifestantes abren paso a camion de bomberos.mp4",
+        "infiltrados detienen a manifestante que los descubre.mp4",
+        "carabineros atropella a una persona en Lampa.mp4",
+        "auga comienza a fluir en rio.mp4",
+        "valparaiso noche del 19 oct 2019 2.mp4.mp4",
+        "mujer pregunta a que comisaria y no recibe respuesta1.mp4",
+        "tecnico en construccion increpa a militar.mp4",
+        "mujer pregunta a que comisaria y no recibe respuesta.mp4",
+        "carabineros golpea  a un nino.mp4",
+        "valparaiso noche del 19 oct 2019.mp4",
+        "Hombre con la pierna fracturada recibe patada de carabinero.mp4",
+        "Maria jose guerrero detenida.mp4",
+        "homecenter coquimbo.mp4",
+        "micro abandonada en santiago.mp4"
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.video-box {
+  margin-bottom: 10px;
+}
+
+.video-box video {
+  width: 300px;
+}
+</style>
